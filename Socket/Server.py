39908b702bfed31e4fd04cc0e60bf1c42fd222d9
bhargav <bhargavbhegde7@gmail.com>
2016-06-10 20:21:57 +0530

    comments added

--- Socket/Server.py
+++ Socket/Server.py
@@ -5,12 +5,6 @@ from thread import *
 import logging
 import sys
 
-def getIp(interface):
-    import netifaces as ni
-    ni.ifaddresses(interface)
-    ip = ni.ifaddresses(interface)[2][0]['addr']
-    return ip
-
 def runCommand(command,):
     print "executing ", command
     print "done running . . . "
@@ -21,17 +15,17 @@ def clientThread(conn,addr):
     print "Got connection from : " + str(addr)
     while True:
         try:
-            message = str(conn.recv(1024)).strip() #number of bytes
+            message = str(conn.recv(1024)).strip() #1024 bytes
             print message," by ",addr
             if not message or message.lower()=='quit':
                 print "Terminating connection . . . "
-                break
-            else:
+                break   #connection closes as soon as break from the loop
+            else:   #incoming normal command message
                 try:
-                    start_new_thread(runCommand,(message,))
+                    start_new_thread(runCommand,(message,)) #run the task inside a new thread
                 except:
                     print "Exception in running command thread . . .", "Terminating connection . . . "
-                    break;
+                    break
         except:
             print "Exception in client thread . . .", "Terminating connection . . . "
             break
@@ -40,16 +34,16 @@ def clientThread(conn,addr):
     return
 
 def main(argv):
-    port = int(argv[0])
-    host = ''
+    port = int(argv[0])             #port from command line argument
+    host = ''                       #'localhost' or '127.0.0.1' or '' are all same
     sock = socket.socket()
     sock.bind((host, port))
-    sock.listen(1)                 #only one client
-    conn = None
+    sock.listen(1)                  #only one client
+    conn = None                     #to access conn outside the loop
     while True:
         print "Waiting for connection . . . \n"
-        conn, addr = sock.accept()
-        start_new_thread(clientThread,(conn,addr))
+        conn, addr = sock.accept()  #waits untill a client connects
+        start_new_thread(clientThread,(conn,addr))  #start a thread for every new client
 
     conn.close()
     sock.close()
